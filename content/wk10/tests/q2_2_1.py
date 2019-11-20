aae07d65606c506066863c06423c101d1c1058f9
Chris Pyles <cpyles@berkeley.edu>
2019-11-20 13:24:28 -0800

    starting textbok

---
+++ content/wk10/tests/q2_2_1.py
@@ -0,0 +1,30 @@
+test = {
+	"name": "q2_2_1",
+	"points": 1,
+	"suites": [ 
+		{
+			"cases": [ 
+				{
+					"code": r"""
+					>>> type(x_val) == float
+					True
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+				{
+					"code": r"""
+					>>> type(y_val) == float
+					True
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+			],
+			"scored": False,
+			"setup": "",
+			"teardown": "",
+			"type": "doctest"
+		}, 
+	]
+}
---
+++ content/wk10/tests/q3_1.py
@@ -0,0 +1,22 @@
+test = {
+	"name": "q3_1",
+	"points": 1,
+	"suites": [ 
+		{
+			"cases": [ 
+				{
+					"code": r"""
+					>>> bool(.3 <= gini <= .7)
+					True
+					""",
+					"hidden": False,
+					"locked": False,
+				}, 
+			],
+			"scored": False,
+			"setup": "",
+			"teardown": "",
+			"type": "doctest"
+		}, 
+	]
+}
